0de7d90d83df2459fdf4dd06c942e21a66a43fc8
Gustavo Hillesheim <g.hillesheim@edu.pucrs.br>
2025-09-04 18:51:04 -0300

    first commit

---
+++ src/main/java/es/pratica/adocoes/AdocoesApplication.java
@@ -0,0 +1,13 @@
+package es.pratica.adocoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdocoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdocoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/pratica/adocoes/AdocoesApplicationTests.java
@@ -0,0 +1,13 @@
+package es.pratica.adocoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdocoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
