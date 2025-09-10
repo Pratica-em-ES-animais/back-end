ee0d94644020a20d63deb497254986716102d011
Gustavo Hillesheim <g.hillesheim@edu.pucrs.br>
2025-09-10 10:53:22 -0300

    criacao da estrutura base do projeto e da feature de cadastro

---
+++ src/main/java/es/pratica/adocoes/adaptadores/persistencia/interfacesdb/UserRepo.java
@@ -0,0 +1,9 @@
+package es.pratica.adocoes.adaptadores.persistencia.interfacesdb;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import es.pratica.adocoes.adaptadores.persistencia.entidades.UserEntity;
+
+public interface UserRepo extends MongoRepository<Optional<UserEntity>, String>{
+    UserEntity findById(String id);    
+}
