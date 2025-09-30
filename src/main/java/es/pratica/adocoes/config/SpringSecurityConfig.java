aa903a76c79ad7ed85f24b3cc59f5a3b873ff7a8
Gustavo Hillesheim Teixeira <g.hillesheim@edu.pucrs.br>
2025-09-30 12:25:55 -0300

    adding wsl properties and fixing SpringSecurity

--- src/main/java/es/pratica/adocoes/config/SpringSecurityConfig.java
+++ src/main/java/es/pratica/adocoes/config/SpringSecurityConfig.java
@@ -1,21 +1,32 @@
 package es.pratica.adocoes.config;
 
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SpringSecurityConfig {
    
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(requests -> requests.requestMatchers("/api/user/create")
+        http.authorizeHttpRequests(requests -> requests.
+                                                        requestMatchers(
+                                            "/api/user/create",
+                                                        "/swagger-ui/**",
+                                                        "/v3/api-docs/**",
+                                                        "/")
+                                                        .permitAll()
+                                                        .anyRequest()
                                                         .permitAll());
         http.formLogin(df -> df.disable()); 
-        http.httpBasic(df -> {});
+        http.httpBasic(df -> Customizer.withDefaults());
         return http.build();
     }
     @Bean
