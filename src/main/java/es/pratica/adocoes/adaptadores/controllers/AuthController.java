b053aef6cc36ada4a32d666d2059ddee9719b474
Gustavo Hillesheim Teixeira <g.hillesheim@edu.pucrs.br>
2025-10-03 14:05:06 -0300

    login created

---
+++ src/main/java/es/pratica/adocoes/adaptadores/controllers/AuthController.java
@@ -0,0 +1,30 @@
+package es.pratica.adocoes.adaptadores.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import es.pratica.adocoes.aplicacao.casosdeuso.LoginUserUC;
+import es.pratica.adocoes.aplicacao.dtos.UserDto;
+import es.pratica.adocoes.aplicacao.dtos.UserLoginDto;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+    private final LoginUserUC loginUserUC;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody UserLoginDto dto, HttpServletRequest request){
+        UserDto userDto = this.loginUserUC.login(dto);
+        if(userDto == null){
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+        request.getSession(true).setAttribute("user", userDto);
+        return new ResponseEntity<>(userDto,HttpStatus.OK);
+    } 
+}
