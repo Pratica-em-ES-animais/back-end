a9be1e4d697d89e6a8ee52a155b4444917c72dad
rafaprests <rbprestes@hotmail.com>
2025-11-13 15:10:15 -0300

    filter gets data from logged user

--- src/main/java/es/pratica/adocoes/adaptadores/controllers/FilterController.java
+++ src/main/java/es/pratica/adocoes/adaptadores/controllers/FilterController.java
@@ -3,10 +3,16 @@ package es.pratica.adocoes.adaptadores.controllers;
 import es.pratica.adocoes.aplicacao.casosdeuso.FilterPetsUC;
 import es.pratica.adocoes.dominio.modelos.AnimalModel;
 import es.pratica.adocoes.aplicacao.dtos.UserDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.List;
 import java.util.Map;
 import es.pratica.adocoes.dominio.servicos.AnimalService;
+import jakarta.servlet.http.HttpServletRequest;
+
+
 @RestController
 @RequestMapping("api/filter")
 public class FilterController {
@@ -20,13 +26,19 @@ public class FilterController {
     }
 
     @PostMapping
-    public List<Map<String, Object>> filtrar() {
+    public List<Map<String, Object>> filtrar(HttpServletRequest request) {
         // simular usuário logado (substituir depois por SecurityContext)
-        UserDto userMock = new UserDto("02783985020", "rafa", "franca", "rafa@franca.com", "51", "999999999", "senha123");
+        //UserDto userMock = new UserDto("02783985020", "rafa", "franca", "rafa@franca.com", "51", "999999999", "senha123");
         
+        UserDto user = (UserDto) request.getSession().getAttribute("user");
+        
+        if(user == null){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Usuário não autenticado");
+        }
+
         // get all dos pets
         List<AnimalModel> pets = animalService.getAll();
         
-        return useCase.execute(userMock, pets, 3);
+        return useCase.execute(user, pets, 3);
     }
 }
